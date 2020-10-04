b0588c4ca0519a8f84f92f4ceb7da0a3facd7b40
unknown <adasnh@gmail.com>
2020-10-04 14:11:48 +0200

    Step 1,2,3 done

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using GradeBook.GradeBooks;
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+
+        }
+        public void GetLetterGrade()
+        {
+
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using GradeBook.GradeBooks;
+
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook  : BaseGradeBook 
+    {
+        
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+         
+        }
+
+    }
+}
